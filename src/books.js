5299ca3501d2be02c700f14ab52a8fb30c33382d
Eugénie Adelin <eugenie.aimee@hotmail.fr>
2026-03-22 22:23:37 +0100

    try a change

--- src/books.js
+++ src/books.js
@@ -39,49 +39,46 @@
 
 // Your code here:
 const booksArray =[
-  {
-    Book1: {
+
+{
    title: "The Old Man and the Sea",
    pages: 128,
    author: "Ernest Hemingway",
    details: {
       language: "English",
       description: "One of Hemingway's most famous works, it tells the story of Santiago...",
-  },},
+  },
 },
 
 {
-  Book2: { 
   title: "The Airbnb Story",
   pages: 256,
    author: "Leight Gallagher",
    details: {
       language: "English",
       description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+   },
 },
-},},
 
 {
-Book3: {
   title: "Educated - A Memoir",
    pages: 352,
    author: "Tara Westover",
    details: {
       language: "English",
       description: "Educated is an account of the struggle for self-invention...",
+   },
 },
-},},
 
 {
-  Book4: { title: "The Art of Learning",
+  title: "The Art of Learning",
   pages: 288,
   author: "Josh Waitzkin",
   details: {
     language: "English",
       description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
   },
-},},
-
+},
 ];
     
 console.log(booksArray[0].title)
